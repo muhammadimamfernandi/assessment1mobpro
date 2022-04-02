ad7eb6ec4a7cb6daf2b55828331b129e3abe6fa6
muhammadimamfernandi <fernandi.bkw@gmail.com>
2022-04-02 21:32:49 +0700

    selesai

--- app/src/main/java/org/d3if2041/hitungkecepatan/MainActivity.kt
+++ app/src/main/java/org/d3if2041/hitungkecepatan/MainActivity.kt
@@ -2,6 +2,8 @@ package org.d3if2041.hitungkecepatan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import org.d3if2041.hitungkecepatan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,32 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button.setOnClickListener { kecepatan() }
+        binding.resetButton.setOnClickListener { reset() }
+    }
+
+    private fun kecepatan() {
+        val jarak = binding.jarakTempuhInp.text.toString()
+        if (TextUtils.isEmpty(jarak)) {
+            Toast.makeText(this, R.string.jarak_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val waktu = binding.waktuTempuhInp.text.toString()
+        if (TextUtils.isEmpty(waktu)) {
+            Toast.makeText(this, R.string.waktu_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val kecepatan = jarak.toFloat() / waktu.toFloat();
+        binding.hasilTextView.text = getString(R.string.hasil, kecepatan)
+    }
+
+    private fun reset() {
+        binding.jarakTempuhInp.text = null
+        binding.waktuTempuhInp.text = null
+
+        binding.hasilTextView.text = null
     }
 }
